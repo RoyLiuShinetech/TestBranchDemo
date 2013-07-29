f561b0bacf7dd51775fa589f1c00a0f232b126d1
RoyLiuShinetech <liurj@shinetechchina.com>
2013-07-29 17:32:28 +0800

    add class 2 and 3

---
+++ TestBranchDemo/Test1/Class3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    class Class3
+    {
+        private void DevelopmentMethod(string develop)
+        {
+        }
+    }
+}
